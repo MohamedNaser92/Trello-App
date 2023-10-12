7081f9615b9253deb352aeda48bf1ba31efb5611
MohamedNaser92 <mohamed.naser.m92@gmail.com>
2023-10-13 01:23:35 +0300

    add prt

--- DB/connection.js
+++ DB/connection.js
@@ -2,9 +2,11 @@ import mongoose from 'mongoose';
 
 const connection = () => {
 	mongoose
-		.connect('mongodb://127.0.0.1:27017/trello-App')
+		.connect(
+			'mongodb+srv://trelloproject:trelloproject@myatlasclusteredu.yqs1cnl.mongodb.net/'
+		)
+		// .connect('mongodb://127.0.0.1:27017/trello-App')
 		.then(() => console.log('DB connected...'))
 		.catch((err) => console.log(`DB error ${err}`));
 };
-
 export default connection;
